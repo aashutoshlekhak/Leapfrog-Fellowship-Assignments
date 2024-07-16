07ed8cdc2f884503ec2a7032432840dd4de8f3bc
Aashutosh Lekhak <lekhakaashutosh218@gmail.com>
2024-07-16 13:40:10 +0545

    bug fixes

---
+++ NodeAssignment6DatabaseConnection/src/database/migrations/20240716075306_rolePermissions.ts
@@ -0,0 +1,52 @@
+import { Knex } from "knex";
+
+const TABLE_NAME = "rolePermissions";
+
+/**
+ * Create table TABLE_NAME.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.bigIncrements();
+    table
+      .integer("role_id", 100)
+      .notNullable()
+      .references("id")
+      .inTable("roles");
+    table
+      .integer("permission_id", 100)
+      .notNullable()
+      .references("id")
+      .inTable("permissions");
+    table.timestamp("created_at").notNullable().defaultTo(knex.raw("now()"));
+
+    table
+      .bigInteger("created_by")
+      .unsigned()
+      .nullable()
+      .references("id")
+      .inTable(TABLE_NAME);
+
+    table.timestamp("updated_at").nullable();
+
+    table
+      .bigInteger("updated_by")
+      .unsigned()
+      .references("id")
+      .inTable(TABLE_NAME)
+      .nullable();
+  });
+}
+
+/**
+ * Drop table TABLE_NAME.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(TABLE_NAME);
+}
